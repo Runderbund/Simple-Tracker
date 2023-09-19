17222c9b9a5f835279173d38d8e13202af408e5b
unknown <sean.herbison@gmail.com>
2023-09-19 10:42:02 -0700

    Added function call and formatted output of summarize_timer.

--- summarize_timer.py
+++ summarize_timer.py
@@ -5,11 +5,11 @@ from datetime import datetime, timedelta
 def summarize():
     with open('timestamps.txt', 'r') as f:
         lines = f.readlines()
-    
+
     start_time = None
     total_time = timedelta()
     interruptions = 0
-    
+
     for line in lines:
         status, timestamp = line.strip().split(',')
         timestamp = datetime.fromisoformat(timestamp)
@@ -20,10 +20,16 @@ def summarize():
             if start_time is not None:
                 interruptions += 1
                 total_time += (timestamp - start_time)
-    
-    summary_text = f"{interruptions} interruptions. {total_time.seconds // 60} minutes {total_time.seconds % 60} seconds off task"
-    
+
+    summary_text = f"{interruptions} interruptions. {total_time.seconds // 60} minutes, {total_time.seconds % 60} seconds off task"
+
+    now = datetime.now()
+    formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+
     with open('summaries.txt', 'a') as f:
-        f.write(f"{datetime.now()}: {summary_text}\n")
+        f.write(f"{formatted_now} - {summary_text}\n")
 
     os.remove('timestamps.txt')
+
+
+summarize()
