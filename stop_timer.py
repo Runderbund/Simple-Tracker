732ca5a88c69ed15adb90570e3948d5f6fbeadca
unknown <sean.herbison@gmail.com>
2023-09-19 10:10:21 -0700

    Broke out functions into separate files, to be more easily assignable to hotkeys. Added a line to remove the file once summarize is run, to reset for the next usage.

---
+++ stop_timer.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def write_timestamp(status):
+    with open('timestamps.txt', 'a') as f:
+        f.write(f"{status},{datetime.now()}\n")
+
+write_timestamp('stop')
---
+++ summarize_timer.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime, timedelta
+# timedelta allows easier time calculations than pulling from the timestamp manually
+
+def summarize():
+    with open('timestamps.txt', 'r') as f:
+        lines = f.readlines()
+    
+    start_time = None
+    total_time = timedelta()
+    interruptions = 0
+    
+    for line in lines:
+        status, timestamp = line.strip().split(',')
+        timestamp = datetime.fromisoformat(timestamp)
+        
+        if status == 'start':
+            start_time = timestamp
+        elif status == 'stop':
+            if start_time is not None:
+                interruptions += 1
+                total_time += (timestamp - start_time)
+    
+    print(f"{interruptions} interruptions. {total_time.seconds // 60} minutes {total_time.seconds % 60} seconds off task")
+
+    os.remove('timestamps.txt')
